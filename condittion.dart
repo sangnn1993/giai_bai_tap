f8b96dc3cab943e3b4f0051230b8baff582acf82
sangnn <sangnn@gmail.com>
2025-02-22 00:11:21 +0700

    2025-02-22 00:11

--- condittion.dart
+++ condittion.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 // Bài 1:Bài tập quản lý sinh viên
 //Solve:
-
+/*
 void main(List<String> args) {
   print("Nhập Họ tên Sinh Viên:");
   var ten = stdin.readLineSync().toString()!;
@@ -40,6 +40,44 @@ void main(List<String> args) {
   print('Thu nhập sau thuế của $ten là $net_profit vnd');
   print('----------------------------------------');
   print({'Họ Tên':ten,'Lương trước thuế':total_income,'Thuế thu nhập':'${tax_rate*100}%','Lương thực lãnh':net_profit});
-}
+}*/
 
 // Bài 2: Quản Lý Bán Hàng
+void main(List<String> args) {
+  print("Nhập Tên Sản Phẩm:");
+  var ten = stdin.readLineSync().toString()!;
+  print("Nhập Số Lượng Mua:");
+  var num = int.parse(stdin.readLineSync().toString()!);
+  print("Nhập Đơn Giá:");
+  var unit_price = double.parse(stdin.readLineSync().toString()!);
+
+  // Tính Tổng Tiền
+  var total = num * unit_price;
+  // Áp dụng giảm giá:
+  // ○ Nếu thành tiền >= 1,000,000, giảm giá 10%.
+  // ○ Nếu thành tiền từ 500,000 đến dưới 1,000,000, giảm giá 5%.
+  // ○ Nếu dưới 500,000, không giảm giá.
+  double discount;
+  switch (total) {
+    case var value when value >= 1000000:
+    discount = 0.1;
+    break;
+    case var value when value >= 500000:
+    discount = 0.05;
+    break;
+    default:
+    discount = 0;
+    break;
+  }
+  var total_2 = total*(1-discount);
+  var vat = total_2*0.08;
+  print('----------------------------------------');
+  print('      ***** Hoá Đơn Bán Hàn *****       ');
+  print('----------------------------------------');
+  print('- ${ten+(' '*(40-ten.length - num.toString().length - 4))}x $num');
+  print('- ${unit_price.toString()+(' '*(40 - unit_price.toString().length - 6))} vnd');
+  print('- $total                             vnd');
+  print('- Giảm ${total*discount}             vnd');
+  print('- VAT: $vat                          vnd');
+  print('- Tổng thanh toán: ${total_2+vat}    vnd');
+}
